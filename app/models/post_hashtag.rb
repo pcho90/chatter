b49a6e29aba7194aa1e89e5b6357590341f13cc9
Peter Cho <pcho51990@gmail.com>
2020-07-20 16:34:31 -0400

    debugging heroku still

--- app/models/post_hashtag.rb
+++ app/models/post_hashtag.rb
@@ -1,4 +1,4 @@
 class PostHashtag < ApplicationRecord
-  belongs_to :post_id
-  belongs_to :hashtag_id
+  belongs_to :post, foreign_key: "post_id"
+  belongs_to :hashtag, foreign_key: "hashtag_id"
 end
