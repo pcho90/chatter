ea0df7b359350a054e2b9b125e2cdf5691e88328
Peter Cho <pcho51990@gmail.com>
2020-07-20 16:38:00 -0400

    please work

--- db/migrate/20200720203022_create_post_hashtags.rb
+++ db/migrate/20200720203022_create_post_hashtags.rb
@@ -1,8 +1,8 @@
 class CreatePostHashtags < ActiveRecord::Migration[6.0]
   def change
     create_table :post_hashtags do |t|
-      t.references :post_id, null: false, foreign_key: true
-      t.references :hashtag_id, null: false, foreign_key: true
+      t.references :post_id, null: false
+      t.references :hashtag_id, null: false
 
       t.timestamps
     end
